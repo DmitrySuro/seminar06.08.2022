809dce91ac63f68ee78c3973fef24334172b0be0
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-08-06 12:22:40 +0300

    seminar c#

---
+++ task/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число
+// и выдаёт его квадрат (число умноженное на само себя).
+
+Console.WriteLine("Введите число");
+int numbers = int.Parse(Console.ReadLine());
+
+int sqr = numbers * numbers;
+
+System.Console.WriteLine(sqr);
+
+double sqr2 = Math.Pow(numbers, 2);
+
+System.Console.WriteLine($"Квадрат числа {numbers} равен - {sqr2}");
